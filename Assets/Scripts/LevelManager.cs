8c69f95c3f71678cf9d94b81042db1c1c5237400
pyk <pyksoft@gmail.com>
2016-11-24 00:41:34 +0100

    set up base project and adds player movement

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+  public void LoadLevel(string levelName) {
+
+    SceneManager.LoadScene(levelName);
+  }
+
+  public void LoadNextLevel() {
+    int indexSC = SceneManager.GetActiveScene().buildIndex;
+    int nextScene = indexSC + 1;
+    SceneManager.LoadScene(nextScene);
+  }
+
+  public void QuitRequest() {
+
+    // Application.Quit(); is an anti-pattern on mobile
+    // the application state should be managed by the OS and the User
+    Application.Quit();
+  }
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  public float speed = 15.0f;
+  public float padding = 0.1f;
+
+  private float xmin;
+  private float xmax;
+  private float ymin;
+  private float ymax;
+
+
+
+  void Start() {
+
+    float distanceZ = transform.position.z - Camera.main.transform.position.z;
+
+    Vector3 leftDownCorner = Camera.main.ViewportToWorldPoint(new Vector3 (0, 0, distanceZ));
+    Vector3 rightUpCorner = Camera.main.ViewportToWorldPoint(new Vector3 (1, 1, distanceZ));
+   
+    xmin = leftDownCorner.x + padding;
+    xmax = rightUpCorner.x - padding;
+    ymin = leftDownCorner.y + padding;
+    ymax = rightUpCorner.y - padding;
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+    MovePlayer();
+  }
+
+  void MovePlayer() {
+ 
+    if (Input.GetKey(KeyCode.UpArrow)) {
+
+      // two methods possible
+      // this.transform.position += new Vector3 (0, speed * Time.deltaTime, 0);
+      this.transform.position += Vector3.up * speed * Time.deltaTime;
+    }
+
+    if (Input.GetKey(KeyCode.DownArrow)) {
+      this.transform.position += Vector3.down * speed * Time.deltaTime;
+
+    }
+
+    if (Input.GetKey(KeyCode.LeftArrow)) {
+      this.transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      this.transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+    // Restrict the position in game
+    float newX = Mathf.Clamp(this.transform.position.x, xmin, xmax);
+    float newY = Mathf.Clamp(this.transform.position.y, ymin, ymax);
+
+    this.transform.position = new Vector3 (newX, newY, this.transform.position.z);
+
+  }
+}
