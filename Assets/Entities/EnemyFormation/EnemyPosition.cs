01816550d126f9e43b47d598f8a9522d26becfaf
pyk <pyksoft@gmail.com>
2016-11-24 23:59:31 +0100

    Adds level1 enemy spawning

---
+++ Assets/Entities/EnemyFormation/EnemyPosition.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPosition : MonoBehaviour {
+
+  void OnDrawGizmos() {
+    Gizmos.DrawWireSphere(transform.position, 1);
+  }
+}
---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+  public GameObject enemyPrefab;
+
+  // Use this for initialization
+  void Start() {
+
+    foreach (Transform child in transform) {
+      GameObject enemy = Instantiate(enemyPrefab, new Vector3 (child.transform.transform.position.x, child.transform.transform.position.y, 0), Quaternion.identity) as GameObject;
+      enemy.transform.parent = child;
+    }
+
+  }
+	
+  // Update is called once per frame
+  void Update() {
+	
+  }
+}
